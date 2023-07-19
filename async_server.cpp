d403da46f108d7fa207ec37938c6d2c7d17527cf
rozhkovaei <116023705+rozhkovaei@users.noreply.github.com>
2023-07-19 15:56:42 +0500

    Update async_server.cpp

--- async_server.cpp
+++ async_server.cpp
@@ -85,7 +85,7 @@ void session::do_read()
 {
 auto self( shared_from_this() );
   socket_.async_read_some( boost::asio::buffer(data_, max_length ),
-    [ this, self ]( boost::system::error_code ec, std::size_t length )
+    [ this, self ]( boost::system::error_code ec, std::size_t /*length*/ )
     {
       if ( !ec )
       {
